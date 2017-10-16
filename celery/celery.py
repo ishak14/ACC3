07b0f627eaa9d21448669088dc0275b8a9510c90
ishak14 <andy.ishak10@gmail.com>
2017-10-16 23:12:56 +0000

    adding flower andd rabbitmq. having problem seeing the worker in flower.

---
+++ celery/celery.py
@@ -0,0 +1,27 @@
+import os
+import random
+import time
+from flask import Flask, request, render_template, session, flash, redirect, \
+    url_for, jsonify
+from flask.ext.mail import Mail, Message
+from celery import Celery
+
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'top-secret!'
+
+
+# Celery configuration
+app.config['CELERY_BROKER_URL'] = 'redis://localhost:6379/0'
+app.config['CELERY_RESULT_BACKEND'] = 'redis://localhost:6379/0'
+
+# Initialize Celery
+celery = Celery(app.name, broker=app.config['CELERY_BROKER_URL'])
+celery.conf.update(app.config)
+
+
+@celery.task
+def send_async_email(msg):
+    """Background task to send an email with Flask-Mail."""
+    with app.app_context():
+        mail.send(msg)
